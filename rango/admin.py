93778053d571400d1b79523771af550485a443cd
Finlay Earsman <2450322E@student.gla.ac.uk>
2021-02-06 17:37:05 +0000

    chapter 3 test pass commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from rango.models import Category
-from rango.models import Page
-from rango.models import UserProfile
 
-admin.site.register(UserProfile)
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
-    
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
+# Register your models here.
